664ff67ac47e7fd5d60b818b193605814f645f6d
donnybrilliant <37931601+donnybrilliant@users.noreply.github.com>
2024-11-26 20:05:04 +0100

    Create Artist model

---
+++ models/Artist.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, Sequelize) => {
+  const Artist = sequelize.define("Artist", {
+    name: {
+      type: Sequelize.DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+
+  Artist.associate = function (models) {
+    Artist.hasMany(models.Product, {
+      foreignKey: { name: "artistId", allowNull: false },
+    });
+    Artist.hasMany(models.Tour, {
+      foreignKey: {
+        name: "artistId",
+        allowNull: false,
+      },
+    });
+    Artist.hasMany(models.Show, {
+      foreignKey: {
+        name: "artistId",
+        allowNull: false,
+      },
+    });
+  };
+
+  return Artist;
+};
