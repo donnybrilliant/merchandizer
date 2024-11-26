b6d8b9b8ab99c928957bebe0c9ba460d2731f193
donnybrilliant <37931601+donnybrilliant@users.noreply.github.com>
2024-11-26 20:04:15 +0100

    Set up database connection

---
+++ models/index.js
@@ -0,0 +1,40 @@
+const Sequelize = require("sequelize");
+const fs = require("fs");
+const path = require("path");
+const basename = path.basename(__filename);
+const connection = {
+  database: process.env.DATABASE_NAME,
+  username: process.env.ADMIN_USERNAME,
+  password: process.env.ADMIN_PASSWORD,
+  port: process.env.DATABASE_PORT,
+  host: process.env.HOST,
+  dialect: process.env.DIALECT,
+  define: {
+    timestamps: false,
+  },
+};
+
+const sequelize = new Sequelize(connection);
+const db = {};
+
+// Load models
+db.sequelize = sequelize;
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    return (
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    );
+  })
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize);
+    db[model.name] = model;
+  });
+
+// Create associations
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+module.exports = db;
