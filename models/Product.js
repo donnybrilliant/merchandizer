deb49f942569869f4e6c867221f13e3aacc75d6c
donnybrilliant <37931601+donnybrilliant@users.noreply.github.com>
2024-11-26 20:05:32 +0100

    Create Product model

---
+++ models/Product.js
@@ -0,0 +1,51 @@
+module.exports = (sequelize, Sequelize) => {
+  const Product = sequelize.define("Product", {
+    name: {
+      type: Sequelize.DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: Sequelize.DataTypes.TEXT,
+    },
+    color: {
+      type: Sequelize.DataTypes.STRING,
+    },
+    size: {
+      type: Sequelize.DataTypes.STRING,
+    },
+    price: {
+      type: Sequelize.DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+    },
+    image: {
+      type: Sequelize.DataTypes.BLOB,
+    },
+  });
+
+  Product.associate = function (models) {
+    Product.belongsTo(models.Category, {
+      foreignKey: "categoryId",
+      allowNull: true,
+      //as: "category",
+    });
+    Product.belongsTo(models.Artist, {
+      foreignKey: {
+        name: "artistId",
+        //as: "artist",
+        allowNull: false,
+      },
+    });
+    Product.belongsToMany(models.Tour, {
+      through: models.TourInventory,
+      foreignKey: "productId",
+      allowNull: true,
+    });
+    Product.belongsToMany(models.Show, {
+      through: models.ShowInventory,
+      foreignKey: "productId",
+      allowNull: false,
+    });
+  };
+
+  return Product;
+};
