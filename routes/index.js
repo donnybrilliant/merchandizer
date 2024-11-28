b9fea4a4bdf15ee995bba0da124d31f3d1ff9ccb
donnybrilliant <37931601+donnybrilliant@users.noreply.github.com>
2024-11-28 12:32:48 +0100

    Apply styles to index

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,10 @@ router.get("/", function (req, res, next) {
   res.send(`
     <html>
     <head>
+    <style>
+     body {width: 100vw; height: 100vh; display: flex; align-items: center; justify-content: center; background: coral; overflow-y: hidden;}
+     h1 {font-family: Arial; font-size: 5rem;}
+     </style>
     <title>Merchandizer</title>
     </head>
       <body>
