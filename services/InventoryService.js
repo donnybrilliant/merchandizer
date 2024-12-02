5589224637d5770c1eadafbea1f31c0731eac3ed
donnybrilliant <37931601+donnybrilliant@users.noreply.github.com>
2024-12-02 20:15:42 +0100

    Create inventory service

---
+++ services/InventoryService.js
@@ -0,0 +1,76 @@
+const { Op } = require("sequelize");
+
+class InventoryService {
+  constructor(db) {
+    this.client = db.sequelize;
+    this.ShowInventory = db.ShowInventory;
+    this.Product = db.Product;
+  }
+
+  // Get all inventory items for a specific show
+  async getAllByShow(showId) {
+    return await this.ShowInventory.findAll({
+      where: { showId },
+      include: [{ model: this.Product, attributes: ["id", "name", "price"] }],
+    });
+  }
+
+  // Get all inventory items for a specific show
+  async getById(showId, productId) {
+    return await this.ShowInventory.findOne({
+      where: { showId, productId },
+      include: [{ model: this.Product, attributes: ["id", "name", "price"] }],
+    });
+  }
+
+  // Create inventory item for a show
+  async create(data) {
+    if (data.endInventory && data.endInventory > data.startInventory) {
+      throw new Error("End inventory cannot be more than start inventory");
+    }
+
+    const existingInventory = await this.ShowInventory.findOne({
+      where: {
+        showId: data.showId,
+        productId: data.productId,
+      },
+    });
+
+    if (existingInventory) {
+      throw new Error("Inventory for this product and show already exists");
+    }
+
+    return await this.ShowInventory.create(data);
+  }
+
+
+  // Update inventory item
+  async update(showId, productId, data) {
+    if (data.endInventory && data.endInventory > data.startInventory) {
+      throw new Error("End inventory cannot be more than start inventory");
+    }
+
+    const existingInventory = await this.ShowInventory.findOne({
+      where: { showId, productId },
+    });
+
+    if (!existingInventory) {
+      throw new Error("Inventory not found");
+    }
+
+    const rowsUpdated = await this.ShowInventory.update(data, {
+      where: { showId, productId },
+    });
+
+    if (!rowsUpdated[0]) return null;
+    return await this.ShowInventory.findOne({ where: { showId, productId } });
+  }
+
+
+  // Delete inventory item
+  async delete(showId, productId) {
+    return await this.ShowInventory.destroy({ where: { showId, productId } });
+  }
+}
+
+module.exports = InventoryService;
