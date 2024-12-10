9467f4cc59f5d14ef0b40ea3ea3f6684712ec715
donnybrilliant <37931601+donnybrilliant@users.noreply.github.com>
2024-12-10 21:31:58 +0100

    Create summarizeAdjustmets util

--- utils/calculation.js
+++ utils/calculation.js
@@ -12,4 +12,33 @@ const calculateAdjustments = (adjustments) => {
   return totals.restock - (totals.giveaway + totals.loss);
 };
 
-module.exports = calculateAdjustments;
+function summarizeAdjustments(adjustments, price) {
+  let restock = 0;
+  let giveaway = 0;
+  let loss = 0;
+  let discount = 0;
+  let totalDiscount = 0;
+
+  for (const adj of adjustments) {
+    if (adj.type === "restock") {
+      restock += adj.quantity;
+    } else if (adj.type === "giveaway") {
+      giveaway += adj.quantity;
+    } else if (adj.type === "loss") {
+      loss += adj.quantity;
+    } else if (adj.type === "discount") {
+      discount += adj.quantity;
+      // Calculate discount amount
+      if (adj.discountType === "percentage") {
+        const discountPerUnit = price * (adj.discountValue / 100);
+        totalDiscount += discountPerUnit * adj.quantity;
+      } else if (adj.discountType === "fixed") {
+        totalDiscount += parseFloat(adj.discountValue) * adj.quantity;
+      }
+    }
+  }
+
+  return { restock, giveaway, loss, discount, totalDiscount };
+}
+
+module.exports = { calculateAdjustments, summarizeAdjustments };
