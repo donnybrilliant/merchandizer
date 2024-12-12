d21e15696837e6bd42200138b1b6c43e2ebaeb54
donnybrilliant <37931601+donnybrilliant@users.noreply.github.com>
2024-12-12 17:01:25 +0100

    Implement role based access for protected routes

--- routes/stats.js
+++ routes/stats.js
@@ -3,23 +3,30 @@ const router = express.Router();
 const db = require("../models");
 const StatsService = require("../services/StatsService");
 const statsService = new StatsService(db);
+const { isAuth, authorize } = require("../middleware/auth");
+
+router.use(isAuth);
 
 // Get stats for a specific show
-router.get("/shows/:id", async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const stats = await statsService.getShowStats(id);
-    res.status(200).json({ success: true, data: stats });
-  } catch (err) {
-    next(err);
+router.get(
+  "/tours/:tourId/shows/:showId",
+  authorize("viewStats"),
+  async (req, res, next) => {
+    try {
+      const { showId } = req.params;
+      const stats = await statsService.getShowStats(showId);
+      res.status(200).json({ success: true, data: stats });
+    } catch (err) {
+      next(err);
+    }
   }
-});
+);
 
 // Get stats for a specific tour
-router.get("/tours/:id", async (req, res, next) => {
+router.get("/tours/:tourId", authorize("viewStats"), async (req, res, next) => {
   try {
-    const { id } = req.params;
-    const stats = await statsService.getTourStats(id);
+    const { tourId } = req.params;
+    const stats = await statsService.getTourStats(tourId);
     res.status(200).json({ success: true, data: stats });
   } catch (err) {
     next(err);
@@ -27,14 +34,21 @@ router.get("/tours/:id", async (req, res, next) => {
 });
 
 // Get stats for a product in a specific tour
-router.get("/tours/:id/products/:productId", async (req, res, next) => {
-  try {
-    const { id, productId } = req.params;
-    const stats = await statsService.getProductStatsForTour(productId, id);
-    res.status(200).json({ success: true, data: stats });
-  } catch (err) {
-    next(err);
+router.get(
+  "/tours/:tourId/products/:productId",
+  authorize("viewStats"),
+  async (req, res, next) => {
+    try {
+      const { tourId, productId } = req.params;
+      const stats = await statsService.getProductStatsForTour(
+        productId,
+        tourId
+      );
+      res.status(200).json({ success: true, data: stats });
+    } catch (err) {
+      next(err);
+    }
   }
-});
+);
 
 module.exports = router;
