bb61c885c59c7f7205eb97d7e2e60bd9d37ea32e
donnybrilliant <37931601+donnybrilliant@users.noreply.github.com>
2024-12-13 19:41:08 +0100

    Refine categories and category service

--- services/CategoryService.js
+++ services/CategoryService.js
@@ -1,8 +1,9 @@
 const { Op } = require("sequelize");
+const createError = require("http-errors");
+const { isSameData } = require("../utils/checks");
 
 class CategoryService {
   constructor(db) {
-    this.client = db.sequelize;
     this.Category = db.Category;
   }
 
@@ -13,7 +14,18 @@ class CategoryService {
 
   // Get category by id
   async getById(id) {
-    return await this.Category.findByPk(id);
+    // Check if category exists
+    const category = await this.Category.findByPk(id);
+    if (!category) throw createError(404, "Category not found");
+    return category;
+  }
+
+  // Check if category name already exists
+  async checkName(name) {
+    const existingCategory = await this.Category.findOne({ where: { name } });
+    if (existingCategory) {
+      throw createError(409, `Category with the name ${name} already exists`);
+    }
   }
 
   // Search for category by name
@@ -29,19 +41,32 @@ class CategoryService {
 
   // Create category
   async create(data) {
+    await this.checkName(data.name);
     return await this.Category.create(data);
   }
 
   // Update category
   async update(id, data) {
-    const rowsUpdated = await this.Category.update(data, { where: { id } });
-    if (!rowsUpdated[0]) return null;
-    return await this.getById(id);
+    // Check if category exists
+    const category = await this.getById(id);
+
+    // Check if the data is the same as the category
+    if (isSameData(category, data)) {
+      return { noChanges: true, data: category };
+    }
+
+    // Check if the name exists
+    await this.checkName(data.name);
+
+    return await category.update(data);
   }
 
   // Delete category
   async delete(id) {
-    return await this.Category.destroy({ where: { id } });
+    // Check if category exists
+    const category = await this.getById(id);
+    await category.destroy();
+    return category;
   }
 }
 
