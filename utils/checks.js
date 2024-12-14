d17b18dcd29fa726854dd8ce1f8044a75e8accf5
donnybrilliant <37931601+donnybrilliant@users.noreply.github.com>
2024-12-14 09:22:19 +0100

    Create util to check that a show is in the dates of a tour

--- utils/checks.js
+++ utils/checks.js
@@ -1,7 +1,19 @@
+const createError = require("http-errors");
+
 // Utility to check if two objects have identical data
 function isSameData(data /*instance */, newData) {
   //const data = instance.get(); // Extract plain object from Sequelize instance
   return Object.entries(newData).every(([key, value]) => data[key] === value);
 }
 
-module.exports = { isSameData };
+function checkDateRange(date, startDate, endDate) {
+  const showDate = new Date(date);
+  const rangeStart = new Date(startDate);
+  const rangeEnd = new Date(endDate);
+
+  if (showDate < rangeStart || showDate > rangeEnd) {
+    throw createError(400, `Show date (${date}) must be between ${startDate} and ${endDate}`);
+  }
+}
+
+module.exports = { isSameData, checkDateRange };
