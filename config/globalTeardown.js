532b999ac3129468d5d6cd34ce20d658a53c0c70
donnybrilliant <37931601+donnybrilliant@users.noreply.github.com>
2024-12-19 18:12:59 +0100

    Refactor global setup and teardown

--- config/globalTeardown.js
+++ config/globalTeardown.js
@@ -2,10 +2,15 @@ const db = require("../models");
 
 module.exports = async () => {
   try {
-    await db.User.destroy({ where: { id: global.testUser.id } });
-    await db.Artist.destroy({ where: { id: global.artistId } });
-    await db.Category.destroy({ where: { id: global.categoryId } });
+    await db.ShowInventory.destroy({
+      where: { showId: global.showId, productId: global.productId },
+    });
+    await db.Show.destroy({ where: { id: global.showId } });
     await db.Tour.destroy({ where: { id: global.tourId } });
+    await db.Category.destroy({ where: { id: global.categoryId } });
+    await db.Product.destroy({ where: { id: global.productId } });
+    await db.Artist.destroy({ where: { id: global.artistId } });
+    await db.User.destroy({ where: { id: global.testUser.id } });
     // Close database connection
     await db.sequelize.close();
     console.log("Global teardown completed");
